16238cabc2679db64a4f6c6bc6498725b3bd2b4e
Jerome Stonebridge <jeromestonebridge@gmail.com>
2025-04-10 05:16:49 -0500

    -

--- config/deploy.rb
+++ config/deploy.rb
@@ -26,7 +26,7 @@ set :branch, 'jeromes'
 # Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
 # run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
 # set :shared_dirs, fetch(:shared_dirs, []).push('public/assets')
-set :shared_files, fetch(:shared_files, []).push('config/secrets.yml', '.env')
+set :shared_files, fetch(:shared_files, []).push('config/database.yml', 'config/secrets.yml', '.env')
 
 # This task is the environment that is loaded for all remote run commands, such as
 # `mina deploy` or `mina rake`.
